d2736d1e700fa0e6973021c7ddd314bd9beeb401
ildarJava <ildar.almakayev@gmail.com>
2015-06-25 09:36:57 +0400

    commited

---
+++ lessons/HumanTest/src/main/java/Human.java
@@ -0,0 +1,29 @@
+/**
+ * Created by aspire on 23.06.2015.
+ */
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age) throws Exception {
+        this.name = name;
+        if(age >= 0)
+            this.age = age;
+        else{
+            throw new Exception("Incorrect number of age!..");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "My name is " + this.name + " " + ", age is " + this.age;
+    }
+}
---
+++ lessons/HumanTest/src/main/java/Program.java
@@ -0,0 +1,11 @@
+/**
+ * Created by aspire on 23.06.2015.
+ */
+import java.lang.Exception;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Human h = new Human("Ildar", 23);
+        System.out.println(h.toString());
+    }
+}
