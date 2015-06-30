3a766588e779be4b551db378c0d5e5ccab30fd32
ildarJava <ildar.almakayev@gmail.com>
2015-06-30 14:37:20 +0400

    Was refactored the project name and was added full checks of lists

--- Students/src/test/java/ru/fsep/lessons/students/StudentsServiceFacadeImplTest.java
+++ Students/src/test/java/ru/fsep/lessons/students/StudentsServiceFacadeImplTest.java
@@ -10,22 +10,25 @@ public class StudentsServiceFacadeImplTest {
     private String fileNameTest;
     private StudentsDaoTxtImpl studentsDaoTxtTest;
     private StudentsServiceFacadeImpl serviceFacadeImplTest;
-    private Student expectedStudent;
+    private ArrayList<Student> expectedListOfStudents;
     private ArrayList<Student> actualListOfStudents;
-    private Student actualStudent;
 
     @Before
     public void setUp() throws Exception {
         fileNameTest = "studentsData.txt";
         studentsDaoTxtTest = new StudentsDaoTxtImpl(fileNameTest);
         serviceFacadeImplTest = new StudentsServiceFacadeImpl(studentsDaoTxtTest);
-        expectedStudent = new Student("Almakayev", "Ildar", 50);
+        expectedListOfStudents = new ArrayList<Student>();
+
+        expectedListOfStudents.add(new Student("Almakayev", "Ildar", 50));
+        expectedListOfStudents.add(new Student("Tiglev", "Semen", 50));
+        expectedListOfStudents.add(new Student("Fedorov", "Yura", 50));
     }
 
     @Test
     public void testGetAll() throws Exception {
         actualListOfStudents = serviceFacadeImplTest.getAll();
-        actualStudent = actualListOfStudents.get(0);
-        Assert.assertEquals(expectedStudent, actualStudent);
+        Assert.assertEquals(expectedListOfStudents, actualListOfStudents);
+
     }
 }
